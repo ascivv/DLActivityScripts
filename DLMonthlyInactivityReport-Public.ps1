e3edef0fbe2e41e57450e0722d2346325f75d7c2
jacobtstafford <39343595+jacobtstafford@users.noreply.github.com>
2018-08-10 16:13:08 -0400

    Publish
    Intitial publish

---
+++ DLMonthlyInactivityReport-Public.ps1
@@ -0,0 +1,78 @@
+<#	
+	.NOTES
+	===========================================================================
+	 Updated on:   	8/10/2018
+	 Created by:   /u/ascIVV
+	===========================================================================
+
+        Exchange Online Powershell is required for message trace.
+		https://docs.microsoft.com/en-us/powershell/exchange/exchange-online/connect-to-exchange-online-powershell/connect-to-exchange-online-powershell?view=exchange-ps
+
+	.DESCRIPTION
+		Compare four weeks of DLWeeklyInactivity report results from your O365 tenant.
+    
+
+#>
+
+
+#Connection info
+$Username = "admin.account@domain.com"
+$PasswordPath = "\\path\to\secure\password.txt"
+
+#Set the constants
+$Date = get-date -format MMddyyyy
+$From = "admin.account@domain.com"
+$To = "helpdesk.email@domain.com"
+$Subject = "Monthly Distribution List Inactivity Report"
+$Body = "See the attached file for distribution lists that have not been emailed in the past 4 weeks. This file has also been saved at " +$DLMonthlyActivityReport+"  so it can be accessed by the Quarterly DL Inactivity Report script. Please only make modifications to the attached file and not any of the weekly or monthly master copies in the share."
+$SMTPServer = "smtp.office365.com"
+$SMTPPort = "587"
+
+#Read the password from the file and convert to SecureString
+$SecurePassword = Get-Content $PasswordPath | ConvertTo-SecureString
+
+#Build a Credential Object from the password file and the $username constant
+$Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $SecurePassword
+
+#Open a session to O365
+$ExOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication  Basic -AllowRedirection
+import-PSSession $ExOSession -AllowClobber
+
+#This will input past four weeks of reports and find lists that are on each report. 
+#Get report run date for previous weekly reports
+$Week1Date = (get-date).AddDays(-21).ToString("MMddyyyy")
+$Week2Date = (get-date).AddDays(-14).ToString("MMddyyyy")
+$Week3Date = (get-date).AddDays(-7).ToString("MMddyyyy")
+$Week4Date = (get-date).ToString("MMddyyyy")
+
+#Set report file path
+$Week1Path = "\\server\share\DL Activity Reports\Inactive\Inactive"+$Week1Date+".txt"
+$Week2Path = "\\server\share\DL Activity Reports\Inactive\Inactive"+$Week2Date+".txt"
+$Week3Path = "\\server\share\DL Activity Reports\Inactive\Inactive"+$Week3Date+".txt"
+$Week4Path = "\\server\share\DL Activity Reports\Inactive\Inactive"+$Week4Date+".txt"
+
+#Input weekly report files
+$Week1Report = Get-Content $Week1Path
+$Week2Report = Get-Content $Week2Path
+$Week3Report = Get-Content $Week3Path
+$Week4Report = Get-Content $Week4Path
+
+#Compare weekly report files
+$Week12Results =  Compare-Object -ReferenceObject $Week1Report -DifferenceObject $Week2Report -ExcludeDifferent -IncludeEqual
+$Week23Results =  Compare-Object -ReferenceObject $Week12Results.InputObject -DifferenceObject $Week3Report -ExcludeDifferent -IncludeEqual
+$Week34Results =  Compare-Object -ReferenceObject $Week23Results.InputObject -DifferenceObject $Week4Report -ExcludeDifferent -IncludeEqual
+
+#Filter slider object out of the results
+$MonthlyInactive = $Week34Results.InputObject
+
+#Set export file name
+$DLMonthlyActivityReport = "\\server\share\DL Activity Reports\Inactive\MonthlyInactive"+$date+".txt"
+
+#Export the findings to a file
+$MonthlyInactive | Out-File $DLMonthlyActivityReport
+
+#Send an email with the findings
+Send-MailMessage -From $From -to $To -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential $Credential -Attachments $DLMonthlyActivityReport
+
+#Close the session to O365
+Remove-PSSession $ExOSession
---
+++ DLWeeklyInactivityReport-Public.ps1
@@ -0,0 +1,65 @@
+<#	
+	.NOTES
+	===========================================================================
+	 Updated on:   	8/10/2018
+	 Created by:   /u/ascIVV
+	===========================================================================
+
+        Exchange Online Powershell is required for message trace.
+		https://docs.microsoft.com/en-us/powershell/exchange/exchange-online/connect-to-exchange-online-powershell/connect-to-exchange-online-powershell?view=exchange-ps
+
+	.DESCRIPTION
+		Generate a basic report of unused Distrobution Lists in your O365 tenant. Save the results and then compare four weeks of results with the DLMonthlyInactivty report.
+    
+
+#>
+
+#Connection info
+$Username = "admin.account@domain.com"
+$PasswordPath = "\\path\to\secure\password.txt"
+
+#Set the constants
+$Date = get-date -format MMddyyyy
+$From = "admin.account@domain.com"
+$To = @("email.one@domain.com","email.two@domain.com")
+$Subject = "Weekly Distribution List Inactivity Report"
+$Body = "This is a confirmation the Weekly DL Inactivity Report script has completed successfully. See the attached file for distribution lists that have not been emailed this week. This file has also been saved at " +$DLActivityReport+" so it can be accessed by the Monthly DL Inactivity Report script."
+$SMTPServer = "smtp.office365.com"
+$SMTPPort = "587"
+
+#Set export file name
+$DLActivityReport = "\\server\share\DL Activity Reports\Inactive"+$Date+".txt"
+
+#Read the password from the file and convert to SecureString
+$SecurePassword = Get-Content $PasswordPath | ConvertTo-SecureString
+
+#Build a Credential Object from the password file and the $username constant
+$Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $SecurePassword
+
+#Open a session to O365
+$ExOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication  Basic -AllowRedirection
+import-PSSession $ExOSession -AllowClobber
+
+#This will run a message activity report on all distribution lists for the past week and output the inactive lists.
+#Get all Distribution Lists.
+$DistroLists = Get-DistributionGroup -ResultSize Unlimited
+
+#Run message trace on each Distribution List to see if it recieved mail in the past x days.
+$DistroListsInUse = $DistroLists | select -ExpandProperty primarysmtpaddress  | Foreach-Object { Get-MessageTrace -RecipientAddress $_ -Status expanded -startdate (Get-Date).AddDays(-8) -EndDate (Get-Date) -pagesize 1| select -first 1} 
+
+#Check to see if the message trace shows recieved mail vs. not returning anything and output active status respectivly. 
+$Results =  Compare-Object -ReferenceObject $DistroListsinUse.RecipientAddress -DifferenceObject $DistroLists.primarySMTPaddress -PassThru
+
+#Export the findings to a file
+$Results | Out-File $DLActivityReport
+
+#Send an email with the findings
+Send-MailMessage -From $From -to $To -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential $Credential -Attachments $DLActivityReport
+
+#Close the session to O365
+Remove-PSSession $ExOSession
+
+
+
+
+
